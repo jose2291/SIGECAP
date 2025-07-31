bacd381bcc15f3c84ec947e42e05e0d3420a18c5
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2025-07-30 22:31:27 -0600

    Create institucion.component.ts

---
+++ src/app/institucion/institucion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-institucion',
+  standalone: true,
+  imports: [],
+  templateUrl: './institucion.component.html',
+  styleUrl: './institucion.component.css'
+})
+export class InstitucionComponent {
+
+}
