fd1b91cb930bd782f81946742b79017ee12a586e
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2025-07-30 22:32:06 -0600

    Update personas-registradas.component.ts

--- src/app/personas-registradas/personas-registradas.component.ts
+++ src/app/personas-registradas/personas-registradas.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PersonaService, Persona } from '../services/persona.service';
 
 @Component({
   selector: 'app-personas-registradas',
+  standalone: true,
+  imports: [CommonModule], // ← ¡IMPORTANTE!
   templateUrl: './personas-registradas.component.html',
   styleUrls: ['./personas-registradas.component.css']
 })
 export class PersonasRegistradasComponent {
-  constructor(private router: Router) {}
+  personas: Persona[] = [];
 
+  constructor(
+    private router: Router,
+    private personaService: PersonaService
+  ) {}
+
+  ngOnInit(): void {
+    this.personaService.obtenerPersonas().subscribe({
+      next: (data) => this.personas = data,
+      error: (err) => console.error('Error cargando personas:', err)
+    });
+  }
+formatFechaVista(fecha: string): string {
+  if (!fecha) return '';
+
+  // Cortar en la T si viene con hora
+  const soloFecha = fecha.split('T')[0];
+
+  const [year, month, day] = soloFecha.split('-');
+  return `${day}/${month}/${year}`;
+}
   goToRegistro(): void {
     this.router.navigate(['/registro-personas']);
   }
