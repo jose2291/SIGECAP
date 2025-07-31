837a87950c2f87cc04828a03268b551f4c5edf91
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2025-07-30 22:30:37 -0600

    Create cargo.component.ts

---
+++ src/app/cargo/cargo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargo',
+  standalone: true,
+  imports: [],
+  templateUrl: './cargo.component.html',
+  styleUrl: './cargo.component.css'
+})
+export class CargoComponent {
+
+}
