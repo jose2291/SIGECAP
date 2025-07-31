c8532b0f2d7564ae68268290d3be73258fae1f42
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2025-07-30 22:32:26 -0600

    Create profesion.component.ts

---
+++ src/app/profesion/profesion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './profesion.component.html',
+  styleUrl: './profesion.component.css'
+})
+export class ProfesionComponent {
+
+}
