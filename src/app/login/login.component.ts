9b79bbedc60e60936b87264ae5bce7c2d0bf0611
jose2291 <josegutierrez2291@gmail.com>
2025-08-07 13:32:33 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,45 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; // ✅ Importa RouterModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule, RouterModule], // ✅ agrega RouterModule
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  mensajeError = '';
 
-  private apiUrl = 'https://localhost:7226/api/login';
-
-  constructor(private http: HttpClient, private router: Router) {}
-
-  onSubmit(): void {
-    if (!this.email || !this.password) {
-      this.mensajeError = '⚠️ Debes ingresar correo y contraseña';
-      return;
-    }
-
-    const body = {
-      correo: this.email,
-      contrasena: this.password
-    };
-
-    this.http.post<any>(this.apiUrl, body).subscribe({
-      next: (resp) => {
-        alert(resp.mensaje);
-        this.router.navigate(['/menu']); // ✅ Redirige al menú principal
-      },
-      error: (err) => {
-        this.mensajeError = err.error?.mensaje || '❌ Error al iniciar sesión';
-      }
-    });
-  }
 }
